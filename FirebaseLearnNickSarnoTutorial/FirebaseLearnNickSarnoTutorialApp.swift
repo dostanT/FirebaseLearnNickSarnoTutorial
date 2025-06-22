e235bcd20aa3dc3559c701c727c84f7b0d751d72
Dostan <turlybekdostan@gmail.com>
2025-06-22 14:44:00 +0500

    Initial Commit

---
+++ FirebaseLearnNickSarnoTutorial/FirebaseLearnNickSarnoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLearnNickSarnoTutorialApp.swift
+//  FirebaseLearnNickSarnoTutorial
+//
+//  Created by Dostan Turlybek on 22.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseLearnNickSarnoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
